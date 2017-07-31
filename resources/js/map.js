f94bd2c5392e2d98e44d345a1c459314a0f57ffb
Pcheluntik <pollypuh@gmail.com>
2017-07-31 08:36:34 +0300

    Страница бара

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,9 +1,8 @@
-
-
 function initMap() {
 //координаты Минска
   var lat = 53.9045398;
   var lng = 27.5615244;
+
 
   var centerLatLng = new google.maps.LatLng(lat, lng);
   var mapOptions = {
@@ -21,9 +20,10 @@ google.maps.event.addDomListener(window, "load", initMap);
 
 
 function addMarker(map, marker_lat, marker_lng) {
+  var image = "../img/marker.svg";
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(marker_lat, marker_lng), // Координаты расположения маркера. В данном случае координаты нашего маркера совпадают с центром карты, но разумеется нам никто не мешает создать отдельную переменную и туда поместить другие координаты.
-    map: map, // Карта на которую нужно добавить маркер
-    icon: 'theatre_icon.png' // (Необязательно) Текст выводимый в момент наведения на маркер
+    map: map,
+    icon: image
   });
 }
