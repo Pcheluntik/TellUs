8136c401d7436aa5f1678160c964241b18f69297
Pcheluntik <pollypuh@gmail.com>
2017-08-07 09:59:03 +0300

    стать партнером

--- resources/js/popap.js
+++ resources/js/popap.js
@@ -1,47 +1,24 @@
 
-var popup = document.querySelectorAll(".popup");
-var link = $("li.profile");
-var body = document.querySelector("body");
+var link = document.querySelectorAll("li.profile.auth");
+var partner = document.querySelectorAll("a.partner");
 
-link[0].addEventListener("click", function(event) {
+popaps();
+function popaps() {
+for (i = 0; i < link.length; i++) {
+  console.log(link[i]);
+link[i].addEventListener("click", function(event) {
   event.preventDefault();
-  if (popup[0].classList.contains("close")) {
-    body.classList.add("overlay");
-    popup[0].classList.remove("close");
-    popup[0].classList.add("open");
-  }
- /*if (storage_name) {
-
-  }
-  if (storage_email) {
-
-  } else {
-
-  }*/
+  $(function(){
+    $('#authorization').arcticmodal();
 });
-/*close.addEventListener("click", function(event) {
+}); };
+
+
+for (i = 0; i < partner.length; i) {
+partner[i].addEventListener("click", function(event) {
   event.preventDefault();
-  body.classList.remove("overlay");
-  popup.classList.remove("open");
-  popup.classList.add("close");
-});*/
-/*form.addEventListener("submit", function(event) {
-  if (!feedback_name.value || !feedback_email.value || !feedback_text.value) {
-    event.preventDefault();
-    popup.classList.remove("feedback-error");
-    popup.offsetWidth = popup.offsetWidth;
-    console.log("Заполните все поля, пожалуйста");
-    popup.classList.add("feedback-error");
-  } else {
-    localStorage.setItem("login", login.value);
-  }
-});*/
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popup[0].classList.contains("open")) {
-      body.classList.remove("overlay");
-      popup[0].classList.remove("open");
-      popup[0].classList.add("close");
-    }
-  }
+  $(function(){
+    $('#partner').arcticmodal();
 });
+}); };
+};
