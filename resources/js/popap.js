5f1d4a66eb77fe91c358d6d06c78ad28efa37970
Pcheluntik <pollypuh@gmail.com>
2017-08-07 11:50:25 +0300

    submenu

--- resources/js/popap.js
+++ resources/js/popap.js
@@ -1,24 +1,23 @@
-
 var link = document.querySelectorAll("li.profile.auth");
 var partner = document.querySelectorAll("a.partner");
-
 popaps();
+
 function popaps() {
-for (i = 0; i < link.length; i++) {
-  console.log(link[i]);
-link[i].addEventListener("click", function(event) {
-  event.preventDefault();
-  $(function(){
-    $('#authorization').arcticmodal();
-});
-}); };
+  for (i = 0; i < link.length; i++) {
+    link[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      $(function() {
+        $('#authorization').arcticmodal();
+      });
+    });
+  };
 
-
-for (i = 0; i < partner.length; i) {
-partner[i].addEventListener("click", function(event) {
-  event.preventDefault();
-  $(function(){
-    $('#partner').arcticmodal();
-});
-}); };
+  for (n = 0; n < 2; n++) {
+    partner[n].addEventListener("click", function(event) {
+      event.preventDefault();
+      $(function() {
+        $('#partner').arcticmodal();
+      });
+    });
+  };
 };
