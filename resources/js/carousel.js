289915434223f1685a05124328de9eae10d5913d
Pcheluntik <pollypuh@gmail.com>
2017-08-17 21:17:21 +0300

    11

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,33 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.new-stock_carousel').slick({
     dots: false,
-   infinite: true,
-   speed: 300,
-   slidesToShow: 3,
-   slidesToScroll: 3,
-   draggable: false,
-    responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
 
-      }
-    },
+        }
+      },
 
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
   $('.tegs').slick({
@@ -40,11 +39,11 @@ $(document).ready(function(){
   });
 
 
-  $('.popular-stock_list').imagesLoaded( function() {
+  $('.popular-stock_list').imagesLoaded(function() {
     $('.popular-stock_list').masonry({
       // options
       itemSelector: '.box',
-      columnWidth : '.box',
+      columnWidth: '.box',
       gutter: 10,
       isFitWidth: true
     });
