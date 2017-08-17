ad1e25e2e84a31f080bd70977b653babe5a9112a
Pcheluntik <pollypuh@gmail.com>
2017-08-17 11:43:32 +0300

    flip

--- resources/js/animations.js
+++ resources/js/animations.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
   jQuery('.actor').addClass("hid").viewportChecker({
-  classToAdd: 'vis animated flipInX',
+  classToAdd: 'vis animated jackInTheBox',
   offset: 100
   });
 
   jQuery('.text p').addClass("hid").viewportChecker({
-  classToAdd: 'vis animated flipInX',
+  classToAdd: 'vis animated jackInTheBox',
   offset: 100
   });
   jQuery('.text ul').addClass("hid").viewportChecker({
-  classToAdd: 'vis animated flipInX',
+  classToAdd: 'vis animated jackInTheBox',
   offset: 100
   });
 
